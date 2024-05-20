8c719f297a1c853c2dc912b5f05ea467bae6a0c2
noyon-podder <noyon.podder7@gmail.com>
2024-05-21 01:12:02 +0600

    movie schema create

---
+++ src/app/modules/movies/movies.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import { TMovie, TReview } from "./movies.interface";
+
+const ReviewSchema: Schema = new Schema<TReview>({
+  email: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+});
+
+const MovieSchema: Schema = new Schema<TMovie>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  releaseDate: { type: Date, required: true },
+  genre: { type: String, required: true },
+  isDeleted: { type: Boolean, required: true, default: false },
+  viewCount: { type: Number, required: true, default: 0 },
+  reviews: { type: [ReviewSchema], required: true },
+  slug: { type: String, required: true, unique: true },
+});
+
+export const Movie = mongoose.model<TMovie>("Movie", MovieSchema);
